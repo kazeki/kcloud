eb01db7b19848a79b97b271063106e286f8c6e53
陈锋 <chen.feng.07@163.com>
2017-02-07 17:35:39 +0800

    kcloud home

---
+++ kcloud-home/src/main/java/com/kzk/kcloud/KcloudHomeApplication.java
@@ -0,0 +1,30 @@
+package com.kzk.kcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author kazeki
+ */
+@SpringBootApplication
+public class KcloudHomeApplication {
+	
+	public KcloudHomeApplication(){
+		System.out.println("KcloudHomeApplication >> Created");
+	}
+
+	@Autowired
+	void setEnviroment(Environment env) {
+	    System.out.println("KcloudHomeApplication >> system.greeting: " 
+		        + env.getProperty("system.greeting"));
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Before Run");
+		SpringApplication.run(KcloudHomeApplication.class, args);
+		System.out.println("After Run");
+	}
+	
+}
