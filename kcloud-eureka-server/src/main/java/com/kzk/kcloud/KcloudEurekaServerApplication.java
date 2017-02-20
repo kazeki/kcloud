d1d19a2306f921900c834f5c5e6259e568ad2329
陈锋 <chen.feng.07@163.com>
2017-02-20 13:19:22 +0800

    test

--- kcloud-eureka-server/src/main/java/com/kzk/kcloud/KcloudEurekaServerApplication.java
+++ kcloud-eureka-server/src/main/java/com/kzk/kcloud/KcloudEurekaServerApplication.java
@@ -3,13 +3,17 @@ package com.kzk.kcloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.core.env.Environment;
+
+import com.netflix.discovery.shared.Application;
 
 /**
  * @author kazeki
  */
-@EnableDiscoveryClient
+@SpringBootApplication
+@EnableEurekaServer
 public class KcloudEurekaServerApplication {
 	
 	public KcloudEurekaServerApplication(){
@@ -27,6 +31,9 @@ public class KcloudEurekaServerApplication {
 	public static void main(String[] args) {
 		System.out.println("Before Run");
 		SpringApplication.run(KcloudEurekaServerApplication.class, args);
+		//以下启动方式参见 https://springcloud.cc/spring-cloud-netflix-zhcn.html#spring-cloud-eureka-server
+		//但是按这种方式启动报错，于是改为常见的启动方式，似乎也没什么问题，Web主页可以打开
+		//new SpringApplicationBuilder(Application.class).web(true).run(args);
 		System.out.println("After Run");
 	}
 	
--- kcloud-system-service/src/main/java/com/kzk/kcloud/KcloudSystemServiceApplication.java
+++ kcloud-system-service/src/main/java/com/kzk/kcloud/KcloudSystemServiceApplication.java
@@ -2,14 +2,30 @@ package com.kzk.kcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author kazeki
  */
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
 public class KcloudSystemServiceApplication {
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
 	
 	public KcloudSystemServiceApplication(){
 		System.out.println("KcloudSystemServiceApplication >> Created");
