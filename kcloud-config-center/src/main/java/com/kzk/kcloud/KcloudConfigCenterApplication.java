70e9975145d39cf5e73a31fff7109848a6eeb877
kazeki <chen.feng.07@163.com>
2018-03-13 14:40:20 +0800

    eureka

--- kcloud-config-center/src/main/java/com/kzk/kcloud/KcloudConfigCenterApplication.java
+++ kcloud-config-center/src/main/java/com/kzk/kcloud/KcloudConfigCenterApplication.java
@@ -2,14 +2,15 @@ package com.kzk.kcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author kazeki
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class KcloudConfigCenterApplication {
 
 	public static void main(String[] args) {
