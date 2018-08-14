1e68cde23a5b8d15f24275171f0a1bcd3d922fe0
kazeki <chen.feng.07@163.com>
2018-08-14 14:00:38 +0800

    修改配置

--- kcloud-eureka-server/src/main/java/com/kzk/kcloud/KcloudEurekaServerApplication.java
+++ kcloud-eureka-server/src/main/java/com/kzk/kcloud/KcloudEurekaServerApplication.java
@@ -1,13 +1,8 @@
 package com.kzk.kcloud;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.core.env.Environment;
-
-import com.netflix.discovery.shared.Application;
 
 /**
  * @author kazeki
@@ -15,26 +10,9 @@ import com.netflix.discovery.shared.Application;
 @SpringBootApplication
 @EnableEurekaServer
 public class KcloudEurekaServerApplication {
-	
-	public KcloudEurekaServerApplication(){
-		System.out.println("KcloudEurekaServerApplication >> Created");
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcloudEurekaServerApplication.class, args);
 	}
 
-	@Autowired
-	void setEnviroment(Environment env) {
-	    System.out.println("KcloudEurekaServerApplication >> system.greeting: " 
-		        + env.getProperty("system.greeting"));
-	    System.out.println("KcloudEurekaServerApplication >> server.port: " 
-		        + env.getProperty("server.port"));
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Before Run");
-		SpringApplication.run(KcloudEurekaServerApplication.class, args);
-		//以下启动方式参见 https://springcloud.cc/spring-cloud-netflix-zhcn.html#spring-cloud-eureka-server
-		//但是按这种方式启动报错，于是改为常见的启动方式，似乎也没什么问题，Web主页可以打开
-		//new SpringApplicationBuilder(Application.class).web(true).run(args);
-		System.out.println("After Run");
-	}
-	
 }
--- kcloud-system-service/src/main/java/com/kzk/kcloud/KcloudSystemServiceApplication.java
+++ kcloud-system-service/src/main/java/com/kzk/kcloud/KcloudSystemServiceApplication.java
@@ -1,31 +1,17 @@
 package com.kzk.kcloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author kazeki
  */
-//@SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaClient
-@Controller
 public class KcloudSystemServiceApplication {
-	@RequestMapping("/")
-    public String home() throws Exception {
-		return "forward:/mappings";
-    }
 	public static void main(String[] args) {
-		System.out.println("Before Run");
 		SpringApplication.run(KcloudSystemServiceApplication.class, args);
-		System.out.println("After Run");
 	}
-	
+
 }
